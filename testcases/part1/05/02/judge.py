50c5aa9d4a55af4f22a8a5c2daa4eb6fbbc1fb90
pooiscoding <pooisworking@gmail.com>
2025-12-23 08:49:12 +0800

    fix testcase to meet spec

--- testcases/part1/05/02/judge.py
+++ testcases/part1/05/02/judge.py
@@ -5,12 +5,13 @@ from helper.config import *
 from helper.utils import *
 
 NR_SUBSCRIBERS = 256
-NR_PUBLISHERS = 256
+NR_PUBLISHERS = 128
 CHANNEL_NAME = "ainiikundayo"
 
 
 subscribers = [None] * NR_SUBSCRIBERS
 publishers = [None] * NR_PUBLISHERS
+publishers2 = [None] * NR_PUBLISHERS
 
 def kill_procs(procs):
     for i in range(len(procs)):
@@ -21,6 +22,7 @@ def kill_procs(procs):
 def kill_all_procs():
     kill_procs(subscribers)
     kill_procs(publishers)
+    kill_procs(publishers2)
 
 nr_msgs_per_pub = [random.randint(30, 50) for _ in range(NR_PUBLISHERS)]
 nr_msgs = sum(nr_msgs_per_pub)
@@ -45,11 +47,9 @@ try:
             kill_all_procs()
             sys.exit(JUDGE_WA)
 
-    kill_procs(publishers)
-
     for i in range(NR_PUBLISHERS):
-        publishers[i] = run_command([f"{BINARY_DIR}/pub", CHANNEL_NAME, f"fakefile{i}", str(nr_msgs_per_pub[i] * 4096)], f"publisher{i}")
-        if not wait_for_output(publishers[i], "The publisher node has joined the channel.", MAX_TIMEOUT, f"publisher{i}"):
+        publishers2[i] = run_command([f"{BINARY_DIR}/pub", CHANNEL_NAME, f"fakefile{NR_PUBLISHERS+i}", str(nr_msgs_per_pub[i] * 4096)], f"publisher{NR_PUBLISHERS+i}")
+        if not wait_for_output(publishers2[i], "The publisher node has joined the channel.", MAX_TIMEOUT, f"publisher{NR_PUBLISHERS+i}"):
             kill_all_procs()
             sys.exit(JUDGE_WA)
     
